2a888da70a41bb8f02d3c9649b76eca6cc5d8033
jponharn <jukkrapong.ponharn@nectec.or.th>
2026-02-19 22:02:05 +0700

    refactor: update Firebase configuration to use environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,15 @@ import {
 } from 'lucide-react';
 
 // Firebase Configuration จาก Environment
-const firebaseConfig = JSON.parse(__firebase_config);
+const firebaseConfig = {
+    apiKey: import.meta.env.apiKey,
+    authDomain: import.meta.env.authDomain,
+    projectId: import.meta.env.projectId,
+    storageBucket: import.meta.env.storageBucket,
+    messagingSenderId: import.meta.env.messagingSenderId,
+    appId: import.meta.env.appId,
+    measurementId: import.meta.env.measurementId
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
